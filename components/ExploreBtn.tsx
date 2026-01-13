b81b5cf3812f0a971adb90059bf6500b2887392d
aliasif78 <aliasif1171@gmail.com>
2026-01-13 23:50:50 +0500

    Added basic post hog integration

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,13 +1,31 @@
 "use client";
 
 import Image from "next/image";
+import posthog from "posthog-js";
 
 const ExploreBtn = () => {
+  const handleClick = () => {
+    console.log("Explore");
+    posthog.capture("explore_events_clicked", {
+      location: "hero_section",
+    });
+  };
+
   return (
-    <button type="button" id="explore-btn" onClick={() => console.log("Explore")} className="mt-7 mx-auto">
+    <button
+      type="button"
+      id="explore-btn"
+      onClick={handleClick}
+      className="mt-7 mx-auto"
+    >
       <a href="#events">
         Explore Events
-        <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24} />
+        <Image
+          src="/icons/arrow-down.svg"
+          alt="arrow-down"
+          width={24}
+          height={24}
+        />
       </a>
     </button>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,46 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import posthog from "posthog-js";
 
 const Navbar = () => {
+  const handleLogoClick = () => {
+    posthog.capture("logo_clicked", {
+      destination: "/",
+    });
+  };
+
+  const handleNavClick = (linkName: string, destination: string) => {
+    posthog.capture(`nav_${linkName}_clicked`, {
+      destination: destination,
+    });
+  };
+
   return (
     <header>
       <nav>
-        <Link href="/" className="logo">
+        <Link href="/" className="logo" onClick={handleLogoClick}>
           <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
           <p>Dev Event</p>
         </Link>
 
         <ul>
-          <Link href="/">Home</Link>
-          <Link href="/events">Events</Link>
-          <Link href="/create">Create Event</Link>
+          <Link href="/" onClick={() => handleNavClick("home", "/")}>
+            Home
+          </Link>
+          <Link
+            href="/events"
+            onClick={() => handleNavClick("events", "/events")}
+          >
+            Events
+          </Link>
+          <Link
+            href="/create"
+            onClick={() => handleNavClick("create_event", "/create")}
+          >
+            Create Event
+          </Link>
         </ul>
       </nav>
     </header>
